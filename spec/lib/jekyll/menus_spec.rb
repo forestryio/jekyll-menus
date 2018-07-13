13cae5212625ea66ef736ff839f3fa9186f0afd1
Matt Duffy <matt@forestry.io>
2018-07-13 12:26:39 -0300

    integrate rspec

---
+++ spec/lib/jekyll/menus_spec.rb
@@ -0,0 +1,8 @@
+require 'jekyll'
+require 'jekyll/menus'
+
+RSpec.describe Jekyll::Menus, '#hello_world' do
+    it 'should do something' do
+        pending "but I'm not sure what yet"
+    end
+end
