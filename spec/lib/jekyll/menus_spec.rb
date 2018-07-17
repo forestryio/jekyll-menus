46dfb33d901484884148186042e8e886715c5f4b
Matt Duffy <matt@forestry.io>
2018-07-17 15:24:20 -0300

    cleanup tests

--- spec/lib/jekyll/menus_spec.rb
+++ spec/lib/jekyll/menus_spec.rb
@@ -3,7 +3,7 @@ require 'jekyll/menus'
 
 RSpec.describe Jekyll::Menus, 'basic functionality' do
     before(:each) do
-        @site = double("Jekyll Site", :data => {}, :pages => [], :collections => [])
+        @site = double('Jekyll Site', :data => {}, :pages => [], :collections => [])
     end
 
     it 'should be constructible' do
@@ -15,21 +15,22 @@ RSpec.describe Jekyll::Menus, 'basic functionality' do
     describe 'data menus' do
         it 'should compile the menus in the data' do
             data_menu = {
-                header: {
-                    url: '/api',
-                    title: 'API Documentation',
-                    identifier: 'api'
+                'header' => {
+                    'url' => 'www.example.com',
+                    'title' => 'A Menu',
+                    'identifier' => 'header'
                 }
             }
             jekyll_menus = {
-                :header => [{
-                    :url=>"/api",
-                    :title=>"API Documentation",
-                    :identifier=>"api", "weight"=>-1,
-                    "_frontmatter"=>false
+                'header' => [{
+                    'url' => 'www.example.com',
+                    'title' => 'A Menu',
+                    'identifier' => 'header',
+                    'weight' => -1,
+                    '_frontmatter' => false
                 }]
             }
-            @site.data["menus"] = data_menu
+            @site.data['menus'] = data_menu
 
             menus = Jekyll::Menus.new(@site)
 
@@ -39,113 +40,88 @@ RSpec.describe Jekyll::Menus, 'basic functionality' do
 
     describe 'frontmatter menus' do
 
+        def page_with_menu_definition
+            double(
+                :data => {
+                    'menus' => {
+                        'header' => {
+                            '_frontmatter' => '',
+                            'identifier' => 'header',
+                            'title' => 'A Menu',
+                            'url' => 'www.example.com',
+                            'weight' => nil
+                        }}},
+                :relative_path => nil
+            )
+        end
+
+        def page_with_menu_reference 
+            double(
+                :data => {'menus' => ['header'], 'ext' => 'ext'}, 
+                :url => 'www.example.com', 
+                :path => 'path', 
+                :relative_path => 'relpath'
+            )
+        end
+
+        def expected_well_defined_menu_output
+            { 
+                'header' => [{
+                    '_frontmatter' => nil,
+                    'identifier' => 'header',
+                    'title' => 'A Menu',
+                    'url' => 'www.example.com',
+                    'weight' => -1
+                }]
+            }
+        end
+
+        def expected_default_menu_output
+            {
+                'header' => [{
+                    'url' => 'www.example.com',
+                    'identifier' => 'path',
+                    '_frontmatter' => 'relpath',
+                    'title' => nil,
+                    'weight' => -1
+                }]
+            }
+        end
+
         describe 'in pages' do
             it 'should build a menu defined in the frontmatter' do
-                page_with_menu_definition = double(
-                    :data => {
-                        'menus' => {
-                            'header' => {
-                                '_frontmatter' => '',
-                                'identifier' => 'header',
-                                'title' => 'A Menu',
-                                'url' => 'www.example.com',
-                                'weight' => nil
-                            }}},
-                    :relative_path => nil
-                )
                 @site.pages << page_with_menu_definition
-                jekyll_menus = {"header" => [{
-                    "_frontmatter" => nil,
-                    "identifier" => "header",
-                    "title" => "A Menu",
-                    "url" => "www.example.com",
-                    "weight" => -1
-                }]}
+                
 
                 menus = Jekyll::Menus.new(@site)
 
-                expect(menus.menus).to eq(jekyll_menus)
+                expect(menus.menus).to eq(expected_well_defined_menu_output)
             end
 
             it 'should build defaults from the page when they are not specified in the frontmatter' do
-                page_with_menu_reference = double(
-                    :data => {'menus' => ['main'], 'ext' => 'ext'}, 
-                    :url => 'www.example.com', 
-                    :path => 'path', 
-                    :relative_path => 'relpath'
-                )
                 @site.pages << page_with_menu_reference
-                jekyll_menus = {"main" => [{
-                    "url"=>"www.example.com",
-                    "identifier"=>"path",
-                    "_frontmatter"=>"relpath",
-                    "title"=>nil,
-                    "weight"=>-1
-                }]}
 
                 menus = Jekyll::Menus.new(@site)
 
-                expect(menus.menus).to eq(jekyll_menus)
+                expect(menus.menus).to eq(expected_default_menu_output)
             end
         end
 
         describe 'in collections' do
             it 'should build a menu defined in the frontmatter of member pages' do
-                page_with_menu_definition = double(
-                    :data => {
-                        'menus' => {
-                            'header' => {
-                                '_frontmatter' => '',
-                                'identifier' => 'header',
-                                'title' => 'A Menu',
-                                'url' => 'www.example.com',
-                                'weight' => nil
-                    }}},
-                    :relative_path => nil
-                )
-                @site = double("Jekyll Site", 
-                    :data => {}, 
-                    :pages => [], 
-                    :collections => [ [ "name", double(:docs => [page_with_menu_definition]) ] ]
-                )
-
-                jekyll_menus = {"header" => [{
-                    "_frontmatter" => nil,
-                    "identifier" => "header",
-                    "title" => "A Menu",
-                    "url" => "www.example.com",
-                    "weight" => -1
-                }]}
+                @site.collections << [ 'name', double(:docs => [ page_with_menu_definition ]) ]
 
                 menus = Jekyll::Menus.new(@site)
 
-                expect(menus.menus).to eq(jekyll_menus)
+                expect(menus.menus).to eq(expected_well_defined_menu_output)
             end
 
             it 'should build defaults from the page when they are not specified in the frontmatter' do
-                page_with_menu_reference = double(
-                    :data => {'menus' => ['main'], 'ext' => 'ext'}, 
-                    :url => 'www.example.com', 
-                    :path => 'path', 
-                    :relative_path => 'relpath'
-                )
-                @site = double("Jekyll Site", 
-                    :data => {}, 
-                    :pages => [], 
-                    :collections => [ [ "name", double(:docs => [page_with_menu_reference]) ] ]
-                )
-
-                jekyll_menus = {"main" => [{
-                    "url"=>"www.example.com",
-                    "identifier"=>"path",
-                    "_frontmatter"=>"relpath",
-                    "title"=>nil,
-                    "weight"=>-1
-                }]}
+                @site.collections << [ 'name', double(:docs => [ page_with_menu_reference ]) ]
 
                 menus = Jekyll::Menus.new(@site)
 
-                expect(menus.menus).to eq(jekyll_menus)
+                expect(menus.menus).to eq(expected_default_menu_output)
             end
         end
     end
